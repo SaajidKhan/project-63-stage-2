e359cdc4b99c4c0c0fdeb6bc6d09b213831cd3e7
SaajidKhan <69465011+SaajidKhan@users.noreply.github.com>
2021-01-21 12:16:41 +0530

    Add files via upload

---
+++ HomeScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View ,TouchableOpacity} from 'react-native';
+import Header from 'react-native-elements';
+export default class HomeScreen extends React.Component{
+      
+}
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    inputBoxContainer:{
+        flex:0.3,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    inputBox:{
+        width:'80%',
+        allignSelf:'center',
+        height:'40%'
+    }
+})
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-63-stage-2
+# project-63
